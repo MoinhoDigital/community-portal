69dd7c63a6af0aef9d8528679fe4cda0cc1e1a97
luandro <luandro@gmail.com>
2021-05-19 14:05:55 -0300

    feat: Terminado compra coletiva

--- src/components/CobuyMain.vue
+++ src/components/CobuyMain.vue
@@ -11,25 +11,65 @@
         >
         <v-card-text>
           <div class="my-4 subtitle-1">
-            <v-icon class="mr-4" color="green">mdi-account-clock</v-icon>
-            <span>{{ product.organizers }}</span>
+            <div
+              class="my-4"
+              v-for="organizer in product.organizers"
+              :key="organizer.contact"
+            >
+              <v-btn
+                v-if="product.stage !== 0"
+                fab
+                small
+                target="_blank"
+                :href="
+                  `https://api.whatsapp.com/send?phone=${organizer.contact}`
+                "
+                class="mr-4"
+                color="green"
+              >
+                <v-icon color="white">mdi-message</v-icon>
+              </v-btn>
+              <span>{{ organizer.name }}</span>
+            </div>
           </div>
           <div class="my-4 subtitle-1">
-            <v-icon class="mr-4" :color="product.open ? 'green' : 'orange'"
+            <v-icon
+              class="mr-4"
+              :color="
+                product.stage === 0 ? 'red' : product.open ? 'green' : 'orange'
+              "
               >mdi-clock</v-icon
             >
-            <TimeAgo locale="pt_BR" long :datetime="product.endDate"></TimeAgo>
+            <span v-if="product.stage > 0"
+              >Iniciado a
+              <TimeAgo
+                locale="pt_BR"
+                long
+                :datetime="product.startDate"
+              ></TimeAgo
+            ></span>
+            <span v-else>Fechado</span>
           </div>
-          <v-chip> {{ product.pricePerLot.format() }} por litro </v-chip>
+          <v-chip>
+            {{ product.pricePerLot && product.pricePerLot.format() }} por litro
+          </v-chip>
         </v-card-text>
         <v-card-actions>
-          <v-btn
+          <!-- <v-btn
             color="deep-purple lighten-2"
             text
             @click="console.log('Hey')"
             :disabled="!product.open"
           >
             Participar
+          </v-btn> -->
+          <v-btn
+            color="deep-purple lighten-2"
+            text
+            :href="product.sheetUrl"
+            target="_blank"
+          >
+            Ver planilha
           </v-btn>
         </v-card-actions>
       </v-card>
@@ -38,26 +78,90 @@
       <v-card class="co-cards">
         <v-card-text>
           <v-timeline align-top dense>
-            <v-timeline-item color="green" small>
+            <v-timeline-item
+              :color="product.stage === 1 ? 'orange' : 'green'"
+              small
+            >
               <div>
                 <h4>Pedidos</h4>
-                <time-ago
-                  v-if="product.stage === 1"
-                  long
-                  :refresh="60"
-                  :datetime="product.endDate"
-                  locale="pt_BR"
-                  tooltip
-                ></time-ago>
+                <div v-if="product.stage === 1">
+                  <p>
+                    <span>Encerra em </span>
+                    <TimeAgo
+                      long
+                      :refresh="60"
+                      :datetime="product.endDate"
+                      locale="pt_BR"
+                      tooltip
+                    ></TimeAgo>
+                  </p>
+                  <v-btn
+                    class="my-2"
+                    v-for="(organizer, index) in product.organizers"
+                    :key="index"
+                    target="_blank"
+                    :href="
+                      `https://api.whatsapp.com/send?phone=${organizer.contact}`
+                    "
+                    :color="getColor(index)"
+                  >
+                    <v-icon class="mr-2" color="white">mdi-message</v-icon>
+                    <span class="white--text text-capitalize"
+                      >{{ $vuetify.breakpoint.xs ? "" : "Fazer pedido com" }}
+                      {{ organizer.name }}</span
+                    >
+                  </v-btn>
+                </div>
               </div>
             </v-timeline-item>
-            <v-timeline-item color="orange" small>
+            <v-timeline-item
+              :color="
+                product.stage === 2
+                  ? 'orange'
+                  : product.stage > 2 || product.stage === 0
+                  ? 'green'
+                  : 'grey'
+              "
+              small
+            >
               <div>
                 <h4>Pagamentos</h4>
-                <p v-if="product.stage === 2">{{ product.bankDetails }}</p>
+                <div v-if="product.stage === 2">
+                  <p>
+                    <span>Encerra {{ getDate(product.paymentEnd) }}</span>
+                  </p>
+                  <p v-for="d in product.bankDetails.split('.\s+')" :key="d">
+                    {{ d }}
+                  </p>
+                  <v-btn
+                    class="my-2 mx-2"
+                    v-for="(organizer, index) in product.organizers"
+                    :key="index"
+                    :href="
+                      `https://api.whatsapp.com/send?phone=${organizer.contact}`
+                    "
+                    target="_blank"
+                    :color="getColor(index)"
+                  >
+                    <v-icon class="mr-2" color="white">mdi-message</v-icon>
+                    <span class="white--text text-capitalize"
+                      >{{ $vuetify.breakpoint.xs ? "" : "Enviar para" }}
+                      {{ organizer.name }}</span
+                    >
+                  </v-btn>
+                </div>
               </div>
             </v-timeline-item>
-            <v-timeline-item color="grey" small>
+            <v-timeline-item
+              :color="
+                product.stage === 3
+                  ? 'orange'
+                  : product.stage > 3 || product.stage === 0
+                  ? 'green'
+                  : 'grey'
+              "
+              small
+            >
               <h4>Aguardando entrega</h4>
               <a
                 v-if="product.stage === 3 && product.tracking"
@@ -65,15 +169,23 @@
                 >Rastreamento <v-icon class="ml-4">mdi-truck</v-icon></a
               >
             </v-timeline-item>
-            <v-timeline-item color="grey" small>
+            <v-timeline-item
+              :color="
+                product.stage === 4
+                  ? 'orange'
+                  : product.stage > 4 || product.stage === 0
+                  ? 'green'
+                  : 'grey'
+              "
+              small
+            >
               <div>
                 <h4>Distribuindo</h4>
-                <a
-                  v-if="product.stage === 4 && product.distribution"
-                  :href="product.tracking"
-                  >{{ product.distribution }}
-                  <v-icon class="ml-4">mdi-hubspot</v-icon></a
-                >
+                <h3 v-if="product.stage === 4 && product.distributionHub">
+                  <v-icon color="green" class="ml-4">mdi-map-marker</v-icon>
+
+                  {{ product.distributionHub }}
+                </h3>
               </div>
             </v-timeline-item>
           </v-timeline>
@@ -82,8 +194,18 @@
     </v-col>
     <v-col cols="12" md="12">
       <v-card>
-        <v-card-title>Participantes</v-card-title>
+        <v-card-title
+          >Participantes:
+          {{
+            product.participants && product.participants.length
+          }}</v-card-title
+        >
         <v-divider />
+        <p class="ml-4 caption">
+          <v-icon color="red" small>mdi-exclamation-thick</v-icon> Atenção não
+          pagamento até a data limite ({{ getDate(product.paymentEnd) }}) irá
+          acarretar em abandono do pedido.
+        </p>
         <v-row>
           <v-col cols="12" md="5" class="pl-8">
             <CobuyParticipant :list="firstHalf" />
@@ -93,6 +215,23 @@
             <CobuyParticipant :list="secondHalf" />
           </v-col>
         </v-row>
+        <v-card-subtitle>
+          <v-row v-if="product.stage === 2">
+            <v-col cols="6">
+              <span
+                >Total: <span class="font-weight-black">{{ total }}</span></span
+              >
+            </v-col>
+            <v-col cols="6">
+              <span
+                >Faltam: <span class="font-weight-black">{{ left }}</span></span
+              >
+            </v-col>
+          </v-row>
+          <div>
+            Confirmados: <span class="font-weight-black">{{ confirmed }}</span>
+          </div>
+        </v-card-subtitle>
       </v-card>
     </v-col>
   </v-row>
@@ -100,10 +239,12 @@
 <script>
 import { TimeAgo } from "vue2-timeago";
 import CobuyParticipant from "@/components/CobuyParticipant.vue";
+import colors from "vuetify/lib/util/colors";
+import dayjs from "dayjs";
 
 export default {
   props: {
-    product: Object
+    product: [Promise, Object]
   },
   components: {
     TimeAgo,
@@ -113,34 +254,83 @@ export default {
     selection: 1
   }),
   computed: {
-    amountPople() {
+    total() {
       if (this.product && this.product.participants) {
-        return Math.ceil(this.product.participants.length / 2);
+        return this.product.participants.reduce((prev, curr) => {
+          return curr.dueAmount.add(prev).format();
+        }, 0);
+      } else {
+        return 0;
+      }
+    },
+    confirmed() {
+      if (this.product && this.product.participants) {
+        return this.product.participants
+          .filter(i => i.payed)
+          .reduce((prev, curr) => {
+            return curr.dueAmount.add(prev).format();
+          }, 0);
+      } else {
+        return 0;
+      }
+    },
+    left() {
+      if (this.product && this.product.participants) {
+        return this.product.participants
+          .filter(i => !i.payed)
+          .reduce((prev, curr) => {
+            return curr.dueAmount.add(prev).format();
+          }, 0);
+      } else {
+        return 0;
+      }
+    },
+    amountPeople() {
+      if (this.product && this.product.participants) {
+        let participants = this.product.participants;
+        if (this.product.stage === 0 || this.product.stage > 2)
+          participants = this.product.participants.filter(p => p.payed);
+        return Math.ceil(participants.length / 2);
       } else return 0;
     },
     firstHalf() {
-      if (this.amountPople > 0)
-        return this.product.participants.slice(0, this.amountPople);
+      if (this.amountPeople > 0) {
+        let participants = this.product.participants;
+        if (this.product.stage === 0 || this.product.stage > 2)
+          participants = this.product.participants.filter(p => p.payed);
+        return participants.slice(0, this.amountPeople);
+      }
     },
     secondHalf() {
-      if (this.amountPople > 0) {
-        return this.product.participants.slice(-this.amountPople);
+      if (this.amountPeople > 0) {
+        let participants = this.product.participants;
+        if (this.product.stage === 0 || this.product.stage > 2)
+          participants = this.product.participants.filter(p => p.payed);
+        return participants.slice(-this.amountPeople);
       }
     }
   },
-  methods: {}
+  methods: {
+    getColor(i) {
+      return colors.green["darken" + (i * 2 + 1)];
+    },
+    getDate(date) {
+      const d = new Date(date);
+      return dayjs(d).format("DD/MM");
+    }
+  }
 };
 </script>
 <style scoped>
-@media (min-width: 720px) {
+@media (min-width: 960px) {
   .co-cards {
-    height: 600px;
+    height: 900px;
   }
 }
 
 @media (min-width: 1264px) {
   .co-cards {
-    height: 650px;
+    height: 750px;
   }
 }
 </style>
--- src/lib/fetchStoreSheet.js
+++ src/lib/fetchStoreSheet.js
@@ -1,9 +1,17 @@
-import currency from 'currency.js'
+import currency from "currency.js";
 
 const localStorageKey = `@moinho/cobuy`;
 
+Date.prototype.addDays = function(days) {
+  var date = new Date(this.valueOf());
+  date.setDate(date.getDate() + days);
+  return date;
+}
+
+
 const fetchGoogle = async (id) => {
   const googleUrl = `https://spreadsheets.google.com/feeds/cells/${id}/1/public/full?alt=json`;
+  const sheetUrl = `https://docs.google.com/spreadsheets/d/${id}`;
   const response = await fetch(googleUrl);
   const json = await response.json();
   function parseData(data) {
@@ -29,10 +37,12 @@ const fetchGoogle = async (id) => {
       }
     }, {});
     const splitDate = obj[6][2].split("-");
+    const splitPaymentDate = obj[7][2].split("/");
     const splitStart = splitDate[0].split("/");
     const splitEnd = splitDate[1].split("/");
     const currentYear = new Date().getFullYear();
     const currentDate = new Date();
+    const tomorrow = currentDate.addDays(1)
     const startDate = new Date(
       currentYear,
       parseInt(splitStart[1]) - 1,
@@ -43,37 +53,68 @@ const fetchGoogle = async (id) => {
       parseInt(splitEnd[1]) - 1,
       parseInt(splitEnd[0]) + 1
     );
-    const BRL = value => currency(value, { symbol: 'R$', decimal: ',', separator: '.' });
-    const pricePerLotWithoutFee = BRL(obj[7][2]);
-    const pricePerLot = pricePerLotWithoutFee.add((pricePerLotWithoutFee.divide(10)))
-
-    let participants = []
-    for (let index = 13; index < Object.keys(obj).length + 2; index++) {
-      const amount = obj[index][2].replace(/,/g, '')
+    const paymentEnd = new Date(
+      currentYear,
+      parseInt(splitPaymentDate[1]) - 1,
+      parseInt(splitPaymentDate[0])
+    );
+    const BRL = (value) =>
+      currency(value, { symbol: "R$", decimal: ",", separator: "." });
+    const pricePerLotWithoutFee = BRL(obj[8][2]);
+    const pricePerLot = pricePerLotWithoutFee.add(
+      pricePerLotWithoutFee.divide(10)
+    );
+    const open = currentDate <= endDate;
+    const openPayment = tomorrow > paymentEnd;
+    const tracking = obj[12][2]
+    const getStage = () => {
+      if (open) return 1;
+      else if (openPayment) return 2;
+      else if (!openPayment && tracking) return 3
+      else if (tracking) return 4
+      return 0;
+    };
+    let participants = [];
+    for (let index = 17; index < Object.keys(obj).length + 2; index++) {
+      const amount = obj[index][2].replace(/,/g, "");
       participants.push({
         name: obj[index][1],
         quantity: obj[index][2],
         shipping: obj[index][5],
-        payed: obj[index][6] === 'TRUE' ? true : false,
-        dueAmount: pricePerLot.multiply(amount)
-      })
+        payed: obj[index][6] === "TRUE" ? true : false,
+        dueAmount: pricePerLot.multiply(amount),
+      });
     }
-    participants.sort((a,b) =>  b.payed ? 1 : -1)
+    participants.sort((a, b) => (b.payed ? 1 : -1));
+    let organizers = []
+    for (let index = 2; index < Object.keys(obj[2]).length + 1; index++) {
+      if (obj[2][index]) {
+        const splitContact = obj[2][index].split(':')
+        organizers.push({
+          name: splitContact[0],
+          contact: splitContact[1].replace(/\s/g, '')
+        })
+      }
+    }
     return {
-      organizers: obj[2][2],
+      tracking,
+      distributionHub: obj[13][2],
+      sheetUrl: sheetUrl,
+      organizers,
       // orgnizerFee: obj[4][2],
       name: obj[4][2],
-      link: obj[9][2],
-      image: obj[10][2],
-      seller: obj[6][2],
+      link: obj[10][2],
+      image: obj[11][2],
+      seller: obj[5][2],
       startDate,
       endDate,
-      stage: 2,
-      open: currentDate <= endDate,
+      paymentEnd,
+      stage: getStage(),
+      open,
       pricePerLotWithoutFee,
       pricePerLot,
-      bankDetails: obj[8][2],
-      participants
+      bankDetails: obj[9][2],
+      participants,
     };
   }
   const finalData = parseData(json.feed.entry);
